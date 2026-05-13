4f015113d23590cab217e67ceb3134f744d53f0a
Alchemy <abdullahi2001mohammed@gmail.com>
2026-05-13 09:43:28 +0200

    Permutation in strings

--- ArrStrg.py
+++ ArrStrg.py
@@ -1078,7 +1078,33 @@ def characterReplacement(s, k):
 
     return max_length
 
-print(characterReplacement("AABABBA", 1))
+'NeetCode 18'
+'Permutation in string'
+'''Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+In other words, return true if one of s1's permutations is the substring of s2'''
+
+# Input: s1 = "ab", s2 = "eidbaooo"
+# Output: true
+
+def checkInclusion(s1, s2):
+    if len(s1) > len(s2): return False
+
+    s1_counts = [0] * 26
+    s2_counts = [0] * 26
+
+    for i in range(len(s1)):
+        s1_counts[ord(s1[i]) - ord('a')] += 1
+        s2_counts[ord(s2[i]) - ord('a')] += 1
+
+    if s1_counts == s2_counts:
+        return True
+
+    for i in range(len(s1), len(s2)):
+        s2_counts[ord(s2[i]) - ord('a')] += 1
+        s2_counts[ord(s2[i - len(s1)]) - ord('a')] -= 1
+        if s1_counts == s2_counts:
+            return True
+    return False
 
 
 
