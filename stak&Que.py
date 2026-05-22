e896690713dfa43ef431476a83559125ed798593
Alchemy <abdullahi2001mohammed@gmail.com>
2026-05-22 09:48:12 +0200

    min Stack

--- stak&Que.py
+++ stak&Que.py
@@ -305,12 +305,39 @@ def isValid(s):
 #Time complexity is O(n)
 #Space complexity is O(n) because the stack size can grow linearly with the input size
 
+'NeetCode 22'
+'Design min stack'
+'''Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.'''
 
 
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val )
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
 
 
+obj = MinStack()
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+obj.pop()
 
-
+#Time: O(1) for the minstack operation
 
 
 
