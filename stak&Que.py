d6e500b37d68b955fefa4b226d4fc14f9a7a08ea
Alchemy <abdullahi2001mohammed@gmail.com>
2026-05-23 17:31:57 +0200

    Generate Parentheses

--- stak&Que.py
+++ stak&Que.py
@@ -368,14 +368,42 @@ def evalRPN(tokens):
 #Time: O(2n)
 #Space: O(n)
 
+'NeetCode 24'
+'Generate Parentheses'
+'''Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.'''
 
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
 
+'Backtracking approach'
+def generateParenthesis(n):
+    #only add open parenthesis if open < n
+    #only add a closing parenthesis if open > closed
+    #valid IIF open == closed == n
+    stack = []
+    res = []
 
+    def backtrack(openN, closedN):
+        if openN == closedN == n:
+            res.append("".join(stack))
+            return
 
+        if openN < n:
+            stack.append("(")
+            backtrack(openN + 1, closedN)
+            stack.pop()
 
+        if closedN < openN:
+            stack.append(")")
+            backtrack(openN, closedN + 1)
+            stack.pop()
+    backtrack(0, 0)
+    return res
 
+print(generateParenthesis(2))
 
-
+#Time : O(4ⁿ / √n)
+#Space: O(n)
 
 
 
