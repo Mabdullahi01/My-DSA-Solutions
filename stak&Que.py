edab6553e2ba0840ff8ff58a54a1702fe051a3a4
Alchemy <abdullahi2001mohammed@gmail.com>
2026-05-24 12:57:50 +0200

    Daily temperatures

--- stak&Que.py
+++ stak&Que.py
@@ -400,10 +400,34 @@ def generateParenthesis(n):
     backtrack(0, 0)
     return res
 
-print(generateParenthesis(2))
+
 
 #Time : O(4ⁿ / √n)
 #Space: O(n)
+
+
+'NeetCode 25'
+'Daily temperature'
+'''Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is
+ the number of days you have to wait after the ith day to get a warmer temperature. 
+ If there is no future day for which this is possible, keep answer[i] == 0 instead.'''
+
+# Input: temperatures = [73,74,75,71,69,72,76,73]
+# Output: [1,1,4,2,1,1,0,0]
+'Using stack'
+def dailyTemperatures(temperatures):
+    res = [0] * len(temperatures)
+    stack = [] # pair: [temp, index]
+
+    for i, t in enumerate(temperatures):
+        while stack and t > stack[-1][0]:
+            stackT, stackInd = stack.pop()
+            res[stackInd] = (i - stackInd)
+        stack.append([t, i])
+    return res
+
+#time: O(n)
+#space: O(n)
 
 
 
