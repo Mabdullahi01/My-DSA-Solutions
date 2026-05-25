59adbf5190640bcce8ffe96badcb2aabcbb3fd25
Alchemy <abdullahi2001mohammed@gmail.com>
2026-05-25 13:15:14 +0200

    Car Fleet

--- stak&Que.py
+++ stak&Que.py
@@ -461,6 +461,25 @@ def dailytemperatures(temperatures):
 #time: O(n)
 #space: O(n)
 
+'NeetCode 26'
+'Car Fleet'
+
+# Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+# Output: 3
+
+def carFleet(target, position, speed):
+    pairs = sorted(zip(position, speed), reverse=True)
+
+    stack = []
+    for p, s in pairs:
+        time = (target - p) / s
+        stack.append(time)
+        if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            stack.pop()
+    return len(stack)
+
+# Time: O(nlogn)
+# space: O(n)
 
 
 
