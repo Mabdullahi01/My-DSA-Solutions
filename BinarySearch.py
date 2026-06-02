f9bb35fa778857653ee44a42ee834cad32315515
Alchemy <abdullahi2001mohammed@gmail.com>
2026-06-02 21:13:09 +0200

    Search 2D matrix

--- BinarySearch.py
+++ BinarySearch.py
@@ -75,3 +75,63 @@ def search(nums, target):
 #Time: O(logn)
 #Space: O(1)
 
+'NeetCode 29'
+'Search a 2D matrix'
+'''Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. 
+Integers in each row are sorted from left to right. The first integer of each row is greater than the last integer of the previous row.'''
+
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# Output: true
+
+def searchMatrix(matrix, target):
+    m, n = len(matrix), len(matrix[0])
+
+    top, bot = 0, m - 1
+    while top <= bot:
+        row = (top + bot) // 2
+        if target > matrix[row][-1]:
+            top = row + 1
+        elif target < matrix[row][0]:
+            bot = row - 1
+        else:
+            break
+    if not (top <= bot):
+        return False
+
+    l, r = 0, n - 1
+
+    while(l <= r):
+        mid = (l + r) // 2
+        if target < matrix[row][mid]:
+            r = mid - 1
+        elif target > matrix[row][mid]:
+            l = mid + 1
+        else:
+            return True
+    return False
+
+#Time: O(logm + logn) = O(logmn)
+
+'Or flattening the array, and completing it in one binary search'
+
+def searchMatrix(matrix, target):
+    m, n = len(matrix), len(matrix[0])
+
+    l, r = 0, m * n - 1
+    while l <= r:
+        mid = (l + r) // 2
+        row = mid // n
+        col = mid % n
+
+        val = matrix[row][col]
+
+        if val == target:
+            return True
+        if val < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return False
+
+#Time: O(logm + logn) = O(logmn)
