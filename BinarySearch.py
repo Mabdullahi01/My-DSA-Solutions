dddda09229cb12e0df595a32e468adabe4901ec5
Alchemy <abdullahi2001mohammed@gmail.com>
2026-06-05 22:49:30 +0200

    Min in Rotated sorted Array

--- BinarySearch.py
+++ BinarySearch.py
@@ -209,3 +209,29 @@ def search(nums, target):
     return -1
 
 #Time: O(log n)
+
+'NeetCode 32'
+'Find Minimum in Rotated Sorted Array'
+
+def findMin(nums):
+    res = nums[0]
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        if nums[l] <= nums[r]:
+            res = min(res, nums[l])
+            break
+
+        mid = (l + r) // 2
+        res = min(res, nums[mid])
+
+        if nums[mid] >= nums[l]:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return res
+
+#Time: O(log n)
+
+
+
