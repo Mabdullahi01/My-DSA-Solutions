d9eaec3d0b792cc414aa418480134f20de7234a8
Alchemy <abdullahi2001mohammed@gmail.com>
2026-06-07 12:04:33 +0200

    Median of two sorted arrays

--- BinarySearch.py
+++ BinarySearch.py
@@ -266,4 +266,45 @@ class TimeMap:
         return res
 #Time: O(log n)
 
+'NeetCode 34' # hard
+'Median of Two Sorted Arrays'
+'''Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n))'''
+
+# Input: nums1 = [1, 2, 3, 4], nums2 = [1, 2, 3, 4, 5, 6, 7, 8]
+# Output: 2.00000
+
+def findMedianSortedArrays(nums1, nums2):
+    A, B = nums1, nums2
+    total = len(A) + len(B)
+    half = total // 2
+
+    if len(B) < len(A):
+        A, B = B, A
+
+    l, r = 0, len(A) - 1
+    while True:
+        i = (l + r) // 2 # to get left partition of A
+        j = half - i - 2 # to get left partition of B
+
+        Aleft = A[i] if i >= 0 else float("-inf")
+        Bleft = B[j] if j >= 0 else float("-inf")
+        Aright = A[i + 1] if (i + 1) < len(A) else float('inf')
+        Bright = B[j + 1] if (j + 1) < len(B) else float('inf')
+
+        if Aleft <= Bright and Bleft <= Aright:
+            # if total length is odd
+            if total % 2:
+                return min(Aright, Bright)
+            # if even
+            return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+        elif Aleft > Bright: # move pointers in A
+            r = i - 1
+        else :
+            l = i + 1
+
+#Time: O(log(min(m, n)))
+
+
+
 
