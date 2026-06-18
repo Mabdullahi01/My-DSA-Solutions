848072609b2ce1354509ca3eda249409efdbb99e
Alchemy <abdullahi2001mohammed@gmail.com>
2026-06-18 20:00:03 +0200

    merge K sorted Linked lists

--- Linkedlists.py
+++ Linkedlists.py
@@ -1396,6 +1396,50 @@ class LRUCache:
 # T: O(1)
 
 
+'NeetCode 44'
+'Merge K sorted Linked List'
+'''You are given an array of k linked-lists lists, each linked-list is sorted in ascending order
+Merge all the linked-lists into one sorted linked-list and return it.'''
+class ListNode():
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeKLists(lists):
+    if not lists or len(lists) == 0:
+        return None
+
+    while len(lists) > 1:
+        merged = []
+
+        for i in range(0, len(lists), 2):
+            l1 = lists[i]
+            l2 = lists[i + 1] if (i + 1) < len(lists) else None
+            merged.append(mergeList(l1, l2))
+        lists = merged
+    return lists[0]
+
+
+def mergeList(l1, l2):
+    dummy = ListNode()
+    cur = dummy
+
+    while l1 and l2:
+        if l1.val < l2.val:
+            cur.next = l1
+            l1 = l1.next
+        else:
+            cur.next = l2
+            l2 = l2.next
+        cur = cur.next
+    cur.next = l1 if l1 else l2
+
+    return dummy.next
+
+
+# T: O(N logk)
+
+
 
 
 
