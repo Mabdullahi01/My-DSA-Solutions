11f7082188a5ce98eaec7c649f1d08ddd0e35d53
Alchemy <abdullahi2001mohammed@gmail.com>
2026-06-29 18:46:48 +0200

    Balanced Tree

--- Trees.py
+++ Trees.py
@@ -772,3 +772,33 @@ class Solution:
         return 1 + max(rh, lh)
 
 # T : O(n), M : O(n)
+
+'NeetCode 49'
+'Balanced Binary Tree'
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def isBalanced(root):
+    return dfs_height(root) != -1
+
+
+def dfs_height(root):
+    if not root:
+        return 0
+
+    lh = dfs_height(root.left)
+    if lh == -1: return -1
+
+    rh = dfs_height(root.right)
+    if rh == -1: return -1
+
+    if abs(lh - rh) > 1: return -1
+
+    return 1 + max(lh, rh)
+
+# T: O(n), M: O(n)
