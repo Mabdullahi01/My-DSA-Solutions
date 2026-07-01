a8b7df861be833e41a3274f261cf3df42fee5a5f
Alchemy <abdullahi2001mohammed@gmail.com>
2026-07-01 20:57:14 +0200

    Same tree

--- Trees.py
+++ Trees.py
@@ -802,3 +802,22 @@ def dfs_height(root):
     return 1 + max(lh, rh)
 
 # T: O(n), M: O(n)
+
+'NeetCode 50'
+'Same tree'
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isSameTree(p, q):
+    if not p and not q:
+        return True
+    if not p or not q or p.val != q.val:
+        return False
+
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+
