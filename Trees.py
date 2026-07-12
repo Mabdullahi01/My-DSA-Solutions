fb001ec2e26e86983a7d6cc178293823615a2a74
Alchemy <abdullahi2001mohammed@gmail.com>
2026-07-12 17:47:02 +0200

    Construct from Pre and Inorder Traversal

--- Trees.py
+++ Trees.py
@@ -997,6 +997,30 @@ class Solution:
                 return cur.val
             cur = cur.right
 
+'NC 58'
+'Construct Binary Tree from PreOrder and InOrder Traversal'
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1: ], inorder[mid + 1: ])
+
+        return root
+
+
+
 
 
 
