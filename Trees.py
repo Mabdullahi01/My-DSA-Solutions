0ce2f6d60d30cfd14a7c9dfd0fec53ad32fde1ad
Alchemy <abdullahi2001mohammed@gmail.com>
2026-07-15 21:49:06 +0200

    Implement Trie

--- Trees.py
+++ Trees.py
@@ -1099,6 +1099,44 @@ class Codec:
 
         return dfs()
 
+'NC 61'
+'Implement Trie(Prefix Tree)'
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word):
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+
+    def startsWith(self, prefix):
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
 
 
 
