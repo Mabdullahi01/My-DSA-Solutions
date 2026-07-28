709e1d82e2057362df66c74253996e50abd526bb
Alchemy <abdullahi2001mohammed@gmail.com>
2026-07-28 11:18:26 +0200

    Word Search II

--- Trees.py
+++ Trees.py
@@ -1209,9 +1209,55 @@ def exist(board, word):
     return False
 
 
+'NC 63'
+'Word Search II'
 
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
 
+    def addWord(self, word):
+        cur = self
 
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+class Solution:
+    def findWords(self, board, words):
+        root = TrieNode()
+        for word in words:
+            root.addWord(word)
+
+        ROWS, COLS = len(board), len(board[0])
+        res, visit = set(), set()
+
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 or
+                r == ROWS or c == COLS or
+                board[r][c] not in node.children or (r, c) in visit):
+                return
+
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.endOfWord:
+                res.add(word)
+
+            dfs(r - 1, c, node, word) # up
+            dfs(r + 1, c, node, word) # down
+            dfs(r, c - 1, node, word) # left
+            dfs(r, c + 1, node, word) # right
+            visit.remove((r, c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, "")
+
+        return list(res)
 
 
 
