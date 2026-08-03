a0f675dc2c65fe3b198fae9885ea341a57d558f7
Alchemy <abdullahi2001mohammed@gmail.com>
2026-08-03 19:16:13 +0200

    kth Largest element in a Stream

--- Heap.py
+++ Heap.py
@@ -257,6 +257,23 @@ def findKthLargest(nums, k):
 # Space: O(k)
 
 
+'NC 64'
+'Kth Largest Element in a Stream'
+
+
+class KthLargest:
+
+    def __init__(self, k, nums):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val):
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
 
 
 
